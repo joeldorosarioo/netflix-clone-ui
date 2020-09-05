d911ad667fb0f7eb744c02da0cd2a769c82ebf39
Joel Oliveira <joeldorosarioo@gmail.com>
2020-09-05 16:29:58 -0300

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Tmdb from './Tmdb';
 
-export default App;
+import AppBar from './components/AppBar';
+import FeaturedMovie from './components/FeaturedMovie';
+import MovieRow from './components/MovieRow';
+
+export default () => {
+	const [movieList, setMovieList] = useState([]);
+	const [featuredData, setFeaturedData] = useState(null);
+	const [blackAppBar, setBlackAppBar] = useState(false);
+
+	useEffect(() => {
+		const loadAll = async () => {
+			// Pegando a lista total
+			let list = await Tmdb.getHomeList();
+			setMovieList(list);
+
+			// Pegando o featured (filme em destaque)
+			let originals = list.filter(i => i.slug === 'originals');
+			let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+			
+			let chosen = originals[0].items.results[randomChosen];
+			let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+			setFeaturedData(chosenInfo);			
+		}
+
+		loadAll();
+	}, []);
+
+	useEffect(() => {
+		const scrollListener = () => {
+			if (window.scrollY > 10) {
+				setBlackAppBar(true);
+			} else {
+				setBlackAppBar(false);
+			}
+		}
+
+		window.addEventListener('scroll', scrollListener);
+
+		return () => {
+			window.removeEventListener('scroll', scrollListener);
+		}
+	}, []);
+
+
+	return (
+		<div className="page">
+			<AppBar black={ blackAppBar } />
+
+			{
+				featuredData &&
+					<FeaturedMovie item={ featuredData } />
+			}
+
+			<section className="lists">
+				{
+					movieList.map((item, key) => (
+						<MovieRow key={ key } title={ item.title } items={ item.items } />
+					))
+				}
+			</section>
+
+			<footer>
+				Feito com <span role="img" aria-label="coração">❤️</span> por Joel Oliveira<br />
+				Todos os direitos das imagens são da Netflix.<br />
+				Dados Extraidos do site TheMovieDB.org
+			</footer>
+
+			{ movieList.length <= 0 &&
+				<div className="loading">
+					<img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="Carregando..." />
+				</div>
+			}			
+		</div>
+	);
+}
